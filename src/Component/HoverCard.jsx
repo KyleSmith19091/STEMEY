f54f958f17086a1e5afa7393e8bd9de437c628a0
Kyle Smith <sKorpion19091>
2021-05-04 08:56:06 +0200

    Added 3d hover card component

---
+++ src/Component/HoverCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HoverCard = ({ children }) => {
+    return (
+        <div className="container">
+            <div className="inner">
+                {children}
+            </div>
+        </div>
+    )
+};
+
+
+export default HoverCard
