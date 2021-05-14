0a09843ad6fb40071ce93cb1a4a3badce100ea25
Kyle Smith <sKorpion19091@gmail.com>
2021-05-14 15:59:19 +0200

    ⚡️ Updated Slide Drawer Links

--- src/Component/SlideDrawer.jsx
+++ src/Component/SlideDrawer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 // Libraries
-import { motion } from "framer-motion"
 
 // CSS
 import "../Style/Component/SlideDrawer.css";
@@ -10,17 +9,37 @@ import "../Style/Component/SlideDrawer.css";
 const SlideDrawer = ({ open }) => {
 
     return (
-        <motion.div
+        <div
             className="slide-drawer"
             style={{ transform: open ? "translate(45%,-10%)" : "translate(145%,-10%)", display: open ? "block" : "none" }}
         >
             <ul className="drawer-list">
-                <li className="drawer-list-item"><a href="/">Academics</a></li>
-                <li className="drawer-list-item"><a href="/">Events</a></li>
-                <li className="drawer-list-item"><a href="/">Teams</a></li>
-                <li className="drawer-list-item"><a href="/">Get Involved</a></li>
+                <li className="drawer-list-item">
+                    <p>Academics</p>
+                    <div className="drawer-list-links-container">
+                        <a href="#">Classes</a>
+                        <a href="#">Tutoring</a>
+                        <a href="/tutoring-summer">Tutoring Summer Program</a>
+                    </div>
+                </li>
+                <li className="drawer-list-item">
+                    <p href="/">Events</p>
+                    <div className="drawer-list-links-container">
+                        <a href="#">Volunteer</a>
+                        <a href="#">Join Our Community</a>
+                        <a href="#">Partner With Us</a>
+                    </div>
+                </li>
+                <li className="drawer-list-item">
+                    <p href="/">Get Involved</p>
+                    <div className="drawer-list-links-container">
+                        <a href="#">STEM World I</a>
+                        <a href="#">STEM World II</a>
+                        <a href="#">STEM-Tober</a>
+                    </div>
+                </li>
             </ul>
-        </motion.div>
+        </div>
     )
 }
 
