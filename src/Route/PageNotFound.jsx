afc51f081c6ed6146e943e956980bb6cbaa3e29f
Kyle Smith <sKorpion19091@gmail.com>
2021-05-18 14:49:08 +0200

    ⚡️ Added sick 404 page animation

--- src/Route/PageNotFound.jsx
+++ src/Route/PageNotFound.jsx
@@ -1,23 +1,40 @@
 // React
 import React from 'react';
 
+// External Libraries
+import Lottie from 'react-lottie';
+
+
 // Internal Components
 import HoverCard from "../Component/HoverCard";
 
 // Images
-import { ReactComponent as Error } from "../Img/SVG/error_404.svg";
+import animationData from "../Img/animation/astronaout.json";
 
 // CSS
 import "../Style/Route/PageNotFound.css";
 
 const PageNotFound = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     return (
         <main className="page-not-found">
             <section className="page-not-found-content">
                 <h1>Woops Page Not Found!</h1>
                 <h3>Might as well check this guy out <span role="img" aria-label="hand-down">👇</span></h3>
                 <HoverCard>
-                    <Error className="error-img" />
+                    <Lottie
+                        options={defaultOptions}
+                        width="50vw"
+                        height="50vw"
+                    />
                 </HoverCard>
             </section>
         </main >
