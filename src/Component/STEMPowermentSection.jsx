df317637fba33fe98b585d45ad81d8cb9c4a0ec8
Kyle Smith <sKorpion19091@gmail.com>
2021-05-23 10:59:51 +0200

    ⚡️ Added offerings breakdown to STEMPowerment page

--- src/Component/STEMPowermentSection.jsx
+++ src/Component/STEMPowermentSection.jsx
@@ -3,11 +3,85 @@ import React from 'react'
 
 // External Components
 import { motion } from "framer-motion";
+import ReactFlow from 'react-flow-renderer';
 
 // CSS
 import "../Style/Component/STEMPowermentSection.css";
 
 const STEMPowermentSection = () => {
+    const elements = [
+        {
+            id: '1',
+            data: { label: "⚡️ STEMpowerment Summer 2021" },
+            position: { x: 300, y: 25 },
+            className: "purple-node"
+        },
+        {
+            id: '2',
+            data: { label: "Grades K-6" },
+            position: { x: 100, y: 125 },
+            className: "dark-node"
+        },
+        {
+            id: '3',
+            data: { label: 'Grades 6-B' },
+            position: { x: 550, y: 125 },
+            className: "purple-node"
+        },
+        {
+            id: '4',
+            data: { label: 'Math Classes' },
+            position: { x: 30, y: 250 },
+            className: "blue-node"
+        },
+        {
+            id: '5',
+            data: { label: 'Science Classes' },
+            position: { x: 200, y: 250 },
+            className: "purple-node"
+        },
+        {
+            id: '6',
+            data: { label: 'Math Classes (Algebra/Geometry)' },
+            position: { x: 450, y: 250 },
+            className: "purple-node"
+        },
+        {
+            id: '7',
+            data: { label: 'Science Classes (Bio/Chem/Physics)' },
+            position: { x: 650, y: 250 },
+            className: "blue-node"
+        },
+        {
+            id: '8',
+            data: { label: 'Depending on public interest we may host ...' },
+            position: { x: 310, y: 350 },
+            className: "dark-node"
+        },
+        {
+            id: '9',
+            data: { label: 'Computer Science Classes aka the best thing since sliced bread' },
+            position: { x: 150, y: 500 },
+            className: "dark-node"
+        },
+        {
+            id: '10',
+            data: { label: 'Engineering Classes' },
+            position: { x: 450, y: 500 },
+            className: "dark-node"
+        },
+        // animated edge
+        { id: 'e1-2', source: '1', target: '2', animated: true },
+        { id: 'e2-3', source: '1', target: '3' },
+        { id: 'e2-4', source: '2', target: '4' },
+        { id: 'e3-5', source: '2', target: '5', animated: true },
+        { id: 'e3-6', source: '3', target: '6' },
+        { id: 'e3-7', source: '3', target: '7', animated: true },
+        { id: 'e1-8', source: '1', target: '8' },
+        { id: 'e8-9', source: '8', target: '9', animated: true },
+        { id: 'e8-10', source: '8', target: '10' },
+    ];
+
     return (
         <section className="stempowerment-section">
             <motion.h1 animate={leftHeaderAnim} className="left text">
@@ -42,6 +116,13 @@ const STEMPowermentSection = () => {
                 <dt><span>{'>'}</span> Mock Competitions</dt>
                 <dd>Low-stakes competitions to prepare your child for the rigorous, in-demand Olympiads.</dd>
             </dl>
+
+            <h1 className="center text">Offerings Breakdown (check this out <span role="img" aria-label="lightning-bolt">👇 </span> it's interactive)</h1>
+
+            <div className="flow-container">
+                <ReactFlow elements={elements} />
+            </div>
+
         </section>
     )
 }
--- src/Route/TutoringSummer.jsx
+++ src/Route/TutoringSummer.jsx
@@ -37,7 +37,7 @@ const TutoringSummer = () => {
                     <STEMPowermentSection />
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    <iframe class="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+                    <iframe className="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
                 </TabPanel>
             </div>
         </main>
