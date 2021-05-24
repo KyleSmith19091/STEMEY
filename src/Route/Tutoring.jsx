e4215ba6fe3c561147e9e2e691624777f712a714
Kyle Smith <sKorpion19091@gmail.com>
2021-05-24 17:00:51 +0200

    ⚡️ Added Tutoring Page

---
+++ src/Route/Tutoring.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "../Style/Route/Tutoring.css";
+
+const Tutoring = () => {
+
+    return (
+        <main>
+            <h1 className="center-text">Tutoring</h1>
+            <iframe className="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+        </main>
+    )
+}
+
+export default Tutoring
