972623764a37f227274cc4026a0740dca1c7fd58
Kyle Smith <sKorpion19091@gmail.com>
2021-06-01 11:34:52 +0200

    ⏫ Added New UI Library

--- src/Route/TutoringSummer.jsx
+++ src/Route/TutoringSummer.jsx
@@ -1,44 +1,36 @@
 // React
-import React, { useState } from 'react'
+import React from 'react';
 
 // Internal Components
-import TabPanel from "../Component/TabPanel";
 import STEMPowermentSection from "../Component/STEMPowermentSection";
 
 // External Components
-import { Tabs, Tab } from '@material-ui/core';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
 
 // CSS
 import "../Style/Route/TutoringSummer.css";
 
 const TutoringSummer = () => {
-    const [value, setValue] = useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
 
     return (
         <main className="tutoring-summer">
             <h1 className="tutoring-header"><span className="header-icon" role="img" aria-label="lightning-bolt">⚡️ </span><span>STEM</span>Powerment</h1>
             <div className="tab-container">
-                <Tabs
-                    value={value}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    onChange={handleChange}
-                    aria-label="STEMPowerment Tutoring Summer Program"
-                    variant="fullWidth"
-                >
-                    <Tab label="Overview" />
-                    <Tab label="Register" />
+                <Tabs isFitted>
+                    <TabList>
+                        <Tab>Description</Tab>
+                        <Tab>Register</Tab>
+                    </TabList>
+
+                    <TabPanels>
+                        <TabPanel>
+                            <STEMPowermentSection />
+                        </TabPanel>
+                        <TabPanel>
+                            <iframe className="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+                        </TabPanel>
+                    </TabPanels>
                 </Tabs>
-                <TabPanel value={value} index={0}>
-                    <STEMPowermentSection />
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <iframe className="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
-                </TabPanel>
             </div>
         </main>
     )
