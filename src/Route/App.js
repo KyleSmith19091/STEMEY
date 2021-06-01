0b1bcfa0a56d16892d4f2e91738db76bcc0f8556
Kyle Smith <sKorpion19091@gmail.com>
2021-06-01 15:32:39 +0200

    ⚠️ Fixed Prop Issue with Home Component

--- src/Route/App.js
+++ src/Route/App.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 
 // External Components
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
 
 // Internal Routes
 import Home from "./Home";
@@ -23,28 +22,26 @@ const App = () => {
   const [open, setOpen] = useState(false);
   return (
     <BrowserRouter>
-      <ChakraProvider>
-        <main className="App">
-          <Header open={open} setOpen={setOpen} />
-          <SlideDrawer open={open} />
-          <Switch>
-            <Route path='/' component={Home} exact />
+      <main className="App">
+        <Header open={open} setOpen={setOpen} />
+        <SlideDrawer open={open} />
+        <Switch>
+          <Route path='/' component={() => <Home open={open} />} exact />
 
-            <Route path='/tutoring-summer' component={TutoringSummer} />
+          <Route path='/tutoring-summer' component={TutoringSummer} />
 
-            <Route path='/classes' component={Classes} />
+          <Route path='/classes' component={Classes} />
 
-            <Route path='/tutoring' component={Tutoring} />
+          <Route path='/tutoring' component={Tutoring} />
 
-            <Route path='/test' component={Test} />
+          <Route path='/test' component={Test} />
 
-            <Route path='/stemworld' component={STEMWorld} />
+          <Route path='/stemworld' component={STEMWorld} />
 
-            <Route component={PageNotFound} />
-          </Switch>
-          <Footer />
-        </main>
-      </ChakraProvider>
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </main>
     </BrowserRouter>
   );
 }
