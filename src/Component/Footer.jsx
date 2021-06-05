1fd30d999b75316e521eba965af87bd6becd3900
Kyle Smith <sKorpion19091@gmail.com>
2021-06-05 11:14:25 +0200

    ➕ Added Footer Newsletter form

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,12 @@
 // React
-import React from 'react'
+import React, { useState } from 'react'
 
 // External Components
 import { Link } from "react-router-dom";
+import { Input, InputLeftElement, InputGroup, Button, useToast } from "@chakra-ui/react";
+
+// Internal Components
+import { validateEmail } from "../Controller/ValidateForm";
 
 // Images
 import { ReactComponent as Youtube } from "../Img/Icon/youtubeIcon.svg";
@@ -10,11 +14,47 @@ import { ReactComponent as Instagram } from "../Img/Icon/instagramIcon.svg";
 import { ReactComponent as Linkedin } from "../Img/Icon/linkedin.svg";
 import { ReactComponent as Classroom } from "../Img/Icon/googleClassroomIcon.svg";
 import { ReactComponent as Discord } from "../Img/Icon/discordIcon.svg";
+import { ReactComponent as EmailIcon } from "../Img/Icon/email_icon.svg";
 
 // CSS
 import "../Style/Component/Footer.css";
 
 const Footer = () => {
+
+    const toast = useToast();
+    const [email, setEmail] = useState("");
+    const handleChange = (event) => setEmail(event.target.value);
+
+    const onSubmitHandler = () => {
+        if (validateEmail(email)) {
+            if (sessionStorage.getItem("email") !== null) {
+                toast({
+                    title: "Already joined",
+                    description: "You've already joined the newsletter",
+                    status: "warning",
+                    duration: 9000,
+                    isClosable: true,
+                })
+            } else {
+                sessionStorage.setItem("email", email);
+                toast({
+                    title: "Joined Newsletter",
+                    description: "Thank you for registering!",
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                })
+            }
+        } else {
+            toast({
+                title: "Invalid Email",
+                description: "Please enter a valid email",
+                status: "error",
+                duration: 9000,
+                isClosable: true,
+            });
+        }
+    };
 
     return (
         <footer>
@@ -35,8 +75,17 @@ const Footer = () => {
                     <Classroom />
                 </a>
             </div>
-            <form action="submit" className="col s12">
-
+            <form action="submit">
+                <InputGroup sx={{ margin: "0 1em" }}>
+                    <InputLeftElement
+                        pointerEvents="none"
+                        children={<EmailIcon />}
+                    />
+                    <Input sx={{ borderWidth: "2px", borderColor: "black" }} colorScheme="blackAlpha" variant="outline" placeholder="Enter email for latest news and updates!" type="email" onChange={handleChange} />
+                </InputGroup>
+                <Button colorScheme="purple" variant="solid" onClick={onSubmitHandler}>
+                    Register
+                </Button>
             </form>
             <div className="page-links">
                 <div className="page-dir">
@@ -61,5 +110,7 @@ const Footer = () => {
         </footer >
     )
 }
+
+
 
 export default Footer
