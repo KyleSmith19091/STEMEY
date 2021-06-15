23f43ed30f81aed62c87c540f22ebf6af9c396d7
Kyle Smith <sKorpion19091@gmail.com>
2021-06-15 17:34:43 +0200

    🆕 Added Aspect Ratio Component

--- src/Component/ImpactSection.jsx
+++ src/Component/ImpactSection.jsx
@@ -1,8 +1,14 @@
+// React
 import React from 'react'
 
-import "../Style/Component/ImpactSection.css";
+// External Components
+import { AspectRatio } from "@chakra-ui/react"
 
+// Internal Components
 import WaveSection from "./WaveSection";
+
+// CSS
+import "../Style/Component/ImpactSection.css";
 
 const videoURLS = [
     "https://www.youtube.com/embed/B-RJqKaOC-A",
@@ -22,21 +28,27 @@ const ImpactSection = () => {
                     <div className="impact-number">
                         <h2>400 Students</h2>
                     </div>
-                    <iframe src={videoURLS[0]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <AspectRatio ratio={16 / 9}>
+                        <iframe src={videoURLS[0]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </AspectRatio>
                 </div>
 
                 <div className="impact-container">
                     <div className="impact-number">
                         <h2>300 Lectures</h2>
                     </div>
-                    <iframe src={videoURLS[1]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <AspectRatio ratio={16 / 9}>
+                        <iframe src={videoURLS[1]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </AspectRatio>
                 </div>
 
                 <div className="impact-container">
                     <div className="impact-number">
                         <h2>30K Viewers</h2>
                     </div>
-                    <iframe src={videoURLS[2]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <AspectRatio ratio={16 / 9}>
+                        <iframe src={videoURLS[2]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </AspectRatio>
                 </div>
 
             </div>
