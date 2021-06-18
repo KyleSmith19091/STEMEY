95f01d9419adfab4a3cd2d15f8159b96a4187a34
Aaditya Doiphode <aadid2004@gmail.com>
2021-06-17 23:44:22 -0700

    updated some css folders to modules

--- src/Route/Classes.jsx
+++ src/Route/Classes.jsx
@@ -1,60 +1,73 @@
 // React
-import React from 'react'
+import React from "react";
 
 // Images
 import molbio from "../Img/Photo/MolBio.png";
 
 // CSS
-import "../Style/Route/Classes.css";
+import styles from "../Style/Route/Classes.module.css";
 
 const classesContent = [
-    {
-        name: "Molecular Biology",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
-        imgPath: molbio,
-        emoji: "🧪"
-    },
-    {
-        name: "Molecular Biology",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
-        imgPath: molbio,
-        emoji: "🧪"
-    },
-    {
-        name: "Molecular Biology",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
-        imgPath: molbio,
-        emoji: "🧪"
-    },
-    {
-        name: "Molecular Biology",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
-        imgPath: molbio,
-        emoji: "🧪"
-    },
+  {
+    name: "Molecular Biology",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
+    imgPath: molbio,
+    emoji: "🧪",
+  },
+  {
+    name: "Molecular Biology",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
+    imgPath: molbio,
+    emoji: "🧪",
+  },
+  {
+    name: "Molecular Biology",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
+    imgPath: molbio,
+    emoji: "🧪",
+  },
+  {
+    name: "Molecular Biology",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam earum amet sequi, consequuntur harum explicabo facilis nobis porro. Magnam odio ducimus, porro temporibus beatae nesciunt non fugit neque corrupti exercitationem.",
+    imgPath: molbio,
+    emoji: "🧪",
+  },
 ];
 
 const Classes = () => {
-    return (
-        <main className="classes">
-            <div initial="hidden" animate="show" className="classes-container">
-                {classesContent.map((cl, idx) => {
-                    return (
-                        <div key={idx} className="card">
-                            <div className="card-img-container">
-                                <img src={cl.imgPath} alt="Class" className="card-img" />
-                            </div>
-                            <div className="card-text-container">
-                                <h3 className="card-header"><span role="img" aria-label="mountain">{cl.emoji} </span>{cl.name}</h3>
-                                <p className="card-description">{cl.description}</p>
-                                <button><span><span>Register</span></span></button>
-                            </div>
-                        </div>
-                    );
-                })}
+  return (
+    <main className={styles.classes}>
+      <div initial="hidden" animate="show" className={styles.classesContainer}>
+        {classesContent.map((cl, idx) => {
+          return (
+            <div key={idx} className={styles.card}>
+              <div className={styles.cardImgContainer}>
+                <img src={cl.imgPath} alt="Class" className={styles.cardImg} />
+              </div>
+              <div className={styles.cardTextContainer}>
+                <h3 className={styles.cardHeader}>
+                  <span role="img" aria-label="mountain">
+                    {cl.emoji}{" "}
+                  </span>
+                  {cl.name}
+                </h3>
+                <p className={styles.cardDescription}>{cl.description}</p>
+                <button>
+                  <span>
+                    <span>Register</span>
+                  </span>
+                </button>
+              </div>
             </div>
-        </main>
-    )
-}
+          );
+        })}
+      </div>
+    </main>
+  );
+};
 
-export default Classes
+export default Classes;
--- src/Route/TutoringSummer.jsx
+++ src/Route/TutoringSummer.jsx
@@ -1,39 +1,64 @@
 // React
-import React from 'react';
+import React from "react";
 
 // Internal Components
 import STEMPowermentSection from "../Component/STEMPowermentSection";
 
 // External Components
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 // CSS
-import "../Style/Route/TutoringSummer.css";
+import styles from "../Style/Route/TutoringSummer.module.css";
 
 const TutoringSummer = () => {
+  return (
+    <main className={styles.tutoringSummer}>
+      <h1 className={styles.tutoringHeader}>
+        <span className={styles.headerIcon} role="img" aria-label="lightning-bolt">
+          ⚡️{" "}
+        </span>
+        <span>STEM</span>Powerment
+      </h1>
+      <div className={styles.tabContainer}>
+        <Tabs isFitted variant="soft-rounded" colorScheme="purple">
+          <TabList>
+            <Tab
+              sx={{ textDecoration: "underline", margin: "0 1em" }}
+              _focus={{ ringColor: "transparent" }}
+              _hover={{ transform: "scale(1.05)" }}
+            >
+              Description
+            </Tab>
+            <Tab
+              sx={{ textDecoration: "underline" }}
+              _focus={{ ringColor: "transparent" }}
+              _hover={{ transform: "scale(1.05)" }}
+            >
+              Register
+            </Tab>
+          </TabList>
 
-    return (
-        <main className="tutoring-summer">
-            <h1 className="tutoring-header"><span className="header-icon" role="img" aria-label="lightning-bolt">⚡️ </span><span>STEM</span>Powerment</h1>
-            <div className="tab-container">
-                <Tabs isFitted variant="soft-rounded" colorScheme="purple" >
-                    <TabList>
-                        <Tab sx={{ textDecoration: "underline", margin: "0 1em" }} _focus={{ ringColor: "transparent" }} _hover={{ transform: "scale(1.05)" }}>Description</Tab>
-                        <Tab sx={{ textDecoration: "underline" }} _focus={{ ringColor: "transparent" }} _hover={{ transform: "scale(1.05)" }}>Register</Tab>
-                    </TabList>
+          <TabPanels>
+            <TabPanel>
+              <STEMPowermentSection />
+            </TabPanel>
+            <TabPanel>
+              <iframe
+                className={styles.form}
+                title="Tutoring Form"
+                src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+              >
+                Loading…
+              </iframe>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+    </main>
+  );
+};
 
-                    <TabPanels>
-                        <TabPanel>
-                            <STEMPowermentSection />
-                        </TabPanel>
-                        <TabPanel>
-                            <iframe className="form" title="Tutoring Form" src="https://docs.google.com/forms/d/e/1FAIpQLScqG5-L8qjwcM_JkKjC7N2VpzNuzQ1pBw_fpY2bjq2P1zfg3Q/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </div>
-        </main>
-    )
-}
-
-export default TutoringSummer
+export default TutoringSummer;
