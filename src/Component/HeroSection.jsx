ab099fb1f98f6d1aae66203bcb1a1ad03753a83f
Kyle Smith <sKorpion19091@gmail.com>
2021-06-21 21:12:00 +0200

    ⚡️ Added Simple Parallax effect to hero section

--- src/Component/HeroSection.jsx
+++ src/Component/HeroSection.jsx
@@ -1,6 +1,5 @@
 // React
 import React, { useState, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
 
 // External Components
 import { motion } from "framer-motion";
@@ -20,14 +19,20 @@ import animationData from "../Img/animation/flying-man.json";
 import "../Style/Component/HeroSection.css";
 
 const HeroSection = () => {
-
     const words = ["STEM", "SCIENCE", "TECHNOLOGY", "ENGINEERING", "MATHEMATICS"];
+
+    const [offsetY, setOffsetY] = useState(window.pageYOffset || document.documentElement.scrollTop);
     const [wordIndex, setWordIndex] = useState(0);
 
     const tl = new TimelineLite();
     let titleContentContainer = useRef(null);
     let heroImage = useRef(null);
     let chevronUp = useRef(null);
+
+    const handleScroll = () => {
+        let offset = window.pageYOffset || document.documentElement.scrollTop;
+        setOffsetY(offset);
+    };
 
     useEffect(() => {
         setTimeout(() => {
@@ -36,35 +41,36 @@ const HeroSection = () => {
     });
 
     useEffect(() => {
-
-        const content = titleContentContainer.children;
-        tl.staggerFrom([content], 1, {
-            x: -80,
+        window.addEventListener("scroll", handleScroll);
+        tl.from(titleContentContainer.children[0], 1, {
+            y: -80,
             ease: Power3.easeOut,
             opacity: 0,
-            delay: 0.8
-        }, 0.15).from(heroImage, 1, {
+            delay: 0.2
+        }).from(heroImage, 1, {
             x: 80,
             opacity: 0,
             ease: Power3.easeOut,
         }).to(chevronUp, 1, {
             y: -80,
             opacity: 1,
-            ease: Power3.easeOut
+            ease: Power3.easeOut,
+            delay: 0.2
         });
 
+        return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
     return (
         <section className="hero-section">
-            <div className="hero-title-container" ref={el => titleContentContainer = el}>
+            <div className="hero-title-container" style={{ transform: `translateY(${offsetY * -0.5}px)` }} ref={el => titleContentContainer = el}>
                 <h1>Build your future in <span className="stem-anim">{words[wordIndex]}<br /></span> with
-                    <span className="hero-text"> STEMEY </span>
+                    <span className="hero-text"> STEMEY. </span>
                 </h1>
             </div>
 
             <HoverCard bgColor="transparent">
-                <div className="hero-image-parent-container">
+                <div className="hero-image-parent-container" style={{ transform: `translateY(${offsetY * -0.5}px)` }}>
                     <div ref={el => heroImage = el} className="hero-image-container">
                         <AnimPlayer className="hero-img" data={animationData} />
                     </div>
@@ -77,10 +83,6 @@ const HeroSection = () => {
 
         </section>
     )
-}
-
-HeroSection.propTypes = {
-    open: PropTypes.bool.isRequired
 }
 
 const onChevronClick = (e) => {
