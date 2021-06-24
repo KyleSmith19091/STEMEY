a12e5a39803111d76da8f8d787bd3e841aaad139
Kyle Smith <sKorpion19091@gmail.com>
2021-06-24 11:24:29 +0200

    ⏫ Changed testing data

--- src/Data/StemWorldData.js
+++ src/Data/StemWorldData.js
@@ -1,11 +1,11 @@
 export const EventData = [
 	{
-		"title": "How to make money?",
-		"speaker": "Elon Musk",
+		"title": "Opening Ceremony",
+		"speaker": "Diana Poplacenel",
 		"date": "2020-02-12",
-		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rem exercitationem tenetur. Consequatur nemo hic aperiam voluptas sequi fugiat omnis repudiandae reiciendis saepe, nobis asperiores ipsa laboriosam dolor laudantium dolore",
+		"description": "Opening Ceremony",
 		"videoID": "DlKBSvQfM1c",
-		"speakerImage": require("../Img/Photo/timelineImage.jpg"),
+		"speakerImage": require("../Img/Photo/elyssa_wolter.png"),
 		"eventImage": require("../Img/Photo/timelineImage.jpg")
 	},
 	{
@@ -14,7 +14,7 @@ export const EventData = [
 		"date": "2020-02-12",
 		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rem exercitationem tenetur. Consequatur nemo hic aperiam voluptas sequi fugiat omnis repudiandae reiciendis saepe, nobis asperiores ipsa laboriosam dolor laudantium dolore",
 		"videoID": "DlKBSvQfM1c",
-		"speakerImage": require("../Img/Photo/timelineImage.jpg"),
+		"speakerImage": require("../Img/Photo/elyssa_wolter.png"),
 		"eventImage": require("../Img/Photo/timelineImage.jpg")
 	},
 	{
@@ -22,7 +22,7 @@ export const EventData = [
 		"speaker": "Bill Gates",
 		"date": "2020-02-12",
 		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rem exercitationem tenetur. Consequatur nemo hic aperiam voluptas sequi fugiat omnis repudiandae reiciendis saepe, nobis asperiores ipsa laboriosam dolor laudantium dolore",
-		"videoID": "DlKBSvQfM1c",
+		"videoID": "sWav0WuNMNs",
 		"speakerImage": require("../Img/Photo/timelineImage.jpg"),
 		"eventImage": require("../Img/Photo/timelineImage.jpg")
 	},
