4df6190d00354b3af4c81edba5eaf70e2f423988
Kyle Smith <sKorpion19091@gmail.com>
2021-07-17 17:00:35 +0200

    ⚡️ Updated StemWorld Page design

--- src/Component/EventTimeline.jsx
+++ src/Component/EventTimeline.jsx
@@ -1,55 +1,70 @@
-import React from 'react'
+import React from "react";
 
 import TimelineCard from "../Component/TimelineCard";
 
 import "../Style/Component/EventTimeline.css";
 
-const EventTimeline = ({ day, dayData, setOpen, setExpandedCardContent, headerColor }) => {
+const EventTimeline = ({
+  day,
+  dayData,
+  setOpen,
+  setExpandedCardContent,
+  headerColor,
+}) => {
+  const handleCardClick = (e) => {
+    setOpen(true);
+    setExpandedCardContent(dayData[e.currentTarget.id]);
+  };
 
-	const handleCardClick = (e) => {
-		setOpen(true);
-		setExpandedCardContent(dayData[e.currentTarget.id]);
-	}
+  return (
+    <div className="timeline-container">
+      <h1 className="timeline-header" style={{ color: headerColor }}>
+        <span>► </span>
+        {"Day " + day}
+      </h1>
+      {dayData.map((event, i) => {
+        if (i === 0) {
+          // First card in timeline
+          return (
+            <TimelineCard
+              first
+              onClick={handleCardClick}
+              title={event.title}
+              speaker={event.speaker}
+              date={event.date}
+              id={i}
+              key={i}
+            />
+          );
+        } else if (i === dayData.length - 1) {
+          // Last card in timeline
+          return (
+            <TimelineCard
+              last
+              onClick={handleCardClick}
+              title={event.title}
+              speaker={event.speaker}
+              date={event.date}
+              id={i}
+              key={i}
+            />
+          );
+        } else {
+          // Other cards
+          return (
+            <TimelineCard
+              onClick={handleCardClick}
+              title={event.title}
+              speaker={event.speaker}
+              date={event.date}
+              id={i}
+              key={i}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+};
 
-	return (
-		<div className="timeline-container">
-			<h1 className="timeline-header" style={{ color: headerColor }}><span>► </span>{"Day " + day}</h1>
-			{
-				dayData.map((event, i) => {
-					if (i === 0) { // First card in timeline
-						return (<TimelineCard
-							first
-							onClick={handleCardClick}
-							title={event.title}
-							speaker={event.speaker}
-							date={event.date}
-							id={i}
-							key={i}
-						/>)
-					} else if (i === dayData.length - 1) { // Last card in timeline
-						return (<TimelineCard
-							last
-							onClick={handleCardClick}
-							title={event.title}
-							speaker={event.speaker}
-							date={event.date}
-							id={i}
-							key={i}
-						/>)
-					} else {
-						return (<TimelineCard
-							onClick={handleCardClick}
-							title={event.title}
-							speaker={event.speaker}
-							date={event.date}
-							id={i}
-							key={i}
-						/>)
-					}
-				})
-			}
-		</div>
-	)
-}
-
-export default EventTimeline
+export default EventTimeline;
