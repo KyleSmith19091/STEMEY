78205c5d247c872f6acc2b23dbbe33c000a6e516
Kyle Smith <sKorpion19091@gmail.com>
2021-07-17 17:01:40 +0200

    ⭐️ Moved drawer to its own component

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,117 +1,63 @@
 // React
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from "react";
 
 // External Components
 import { motion } from "framer-motion";
 import { TweenLite, Power3 } from "gsap";
 import { Link } from "react-router-dom";
-import {
-    Drawer,
-    DrawerBody,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    useDisclosure,
-    Button
-} from "@chakra-ui/react";
+import { useDisclosure, Button } from "@chakra-ui/react";
 
 // Internal Components
-import NavItem from "./NavItem";
+import SlideDrawer from "./SlideDrawer";
 
-// Internal Data
-import { Academics, Events, Team, GetInvolved } from "../Model/NavBarContent";
-
-// Images 
+// Images
 import { ReactComponent as Hamburger } from "../Img/Icon/hamburger.svg";
 
 // CSS
 import "../Style/Component/Header.css";
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  let hamburgerRef = useRef(null);
+  let logoRef = useRef(null);
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    let hamburgerRef = useRef(null);
-    let logoRef = useRef(null);
+  useEffect(() => {
+    TweenLite.from(logoRef, 1.4, {
+      y: -200,
+      opacity: 0,
+      ease: Power3.easeOut,
+      delay: 0.2,
+    });
+  }, []);
 
-    useEffect(() => {
-        TweenLite.from(logoRef, 1.4, {
-            y: -200,
-            opacity: 0,
-            ease: Power3.easeOut,
-            delay: 0.2
-        })
-    }, [])
+  return (
+    <header className="header">
+      <div className="logo-container" ref={(el) => (logoRef = el)}>
+        <Link to="/" className="logo">
+          <motion.h1 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            STEMEY.
+          </motion.h1>
+        </Link>
+      </div>
 
-    return (
-        <header className="header">
+      {/* TODO: Move this to SlideDrawer Component */}
+      <div className="menu-icon-container">
+        <Button
+          ref={(el) => (hamburgerRef = el)}
+          colorScheme="transparent"
+          onClick={onOpen}
+          _focus={{ ringColor: "transparent" }}
+        >
+          <Hamburger className="hamburger-icon" />
+        </Button>
+        <SlideDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          focusRef={hamburgerRef}
+        />
+      </div>
+    </header>
+  );
+};
 
-            <div className="logo-container" ref={el => logoRef = el}>
-                <Link to="/" className="logo">
-                    <motion.h1 whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                        STEMEY.
-                    </motion.h1>
-                </Link>
-            </div>
-
-            <nav className="nav" style={{ display: "none" }}>
-                <NavItem name={Academics.name} content={Academics.content} />
-                <NavItem name={Events.name} content={Events.content} />
-                <NavItem name={Team.name} content={Team.content} />
-                <NavItem name={GetInvolved.name} content={GetInvolved.content} />
-            </nav>
-
-            {/* TODO: Move this to SlideDrawer Component */}
-            <div className="menu-icon-container">
-                <Button ref={el => hamburgerRef = el} colorScheme="transparent" onClick={onOpen} _focus={{ ringColor: "transparent" }}>
-                    <Hamburger className="hamburger-icon" />
-                </Button>
-                <Drawer
-                    isOpen={isOpen}
-                    placement="right"
-                    onClose={onClose}
-                    finalFocusRef={hamburgerRef}
-                    size="xlg"
-                    sx={{ backgroundColor: "purple" }}
-                >
-                    <DrawerOverlay />
-                    <DrawerContent>
-                        <DrawerHeader sx={{ backgroundColor: "var(--primary-purple)", color: "white", float: "right" }}><Link onClick={onClose} to="/">STEMEY.</Link></DrawerHeader>
-
-                        <DrawerBody className="drawer-body" sx={{ backgroundColor: "var(--primary-purple)" }}>
-                            <ul className="drawer-list">
-                                <li className="drawer-list-item">
-                                    <p>Academics</p>
-                                    <div className="drawer-list-links-container">
-                                        <Link onClick={onClose} to="/classes">Classes</Link>
-                                        <Link onClick={onClose} to="/tutoring">Tutoring</Link>
-                                        <Link onClick={onClose} to="/tutoring-summer">Tutoring Summer Program</Link>
-                                    </div>
-                                </li>
-                                <li className="drawer-list-item">
-                                    <p>Events</p>
-                                    <div className="drawer-list-links-container">
-                                        <Link onClick={onClose} to="/volunteer">Volunteer</Link>
-                                        <Link onClick={onClose} to="/community">Join Our Community</Link>
-                                        <Link onClick={onClose} to="/partner">Partner With Us</Link>
-                                    </div>
-                                </li>
-                                <li className="drawer-list-item">
-                                    <p>Get Involved</p>
-                                    <div className="drawer-list-links-container">
-                                        <Link onClick={onClose} to="/stemworld">STEM World I</Link>
-                                        <Link onClick={onClose} to="/stemworld2">STEM World II</Link>
-                                        <Link onClick={onClose} to="/test">STEM-Tober</Link>
-                                    </div>
-                                </li>
-                            </ul>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
-            </div>
-        </header >
-    )
-}
-
-export default Header
+export default Header;
--- src/Component/SlideDrawer.jsx
+++ src/Component/SlideDrawer.jsx
@@ -1,49 +1,104 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+// React
+import React from "react";
+
+// PropTypes
+import PropTypes from "prop-types";
 
 // External Components
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    useDisclosure
-} from "@chakra-ui/react"
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+} from "@chakra-ui/react";
+
+import { Link } from "react-router-dom";
 
 // CSS
 import "../Style/Component/SlideDrawer.css";
 
-const SlideDrawer = ({ open, myRef }) => {
-
-    const { isOpen, onOpen, onClose } = useDisclosure();
-
-    return (
-        <Drawer
-            isOpen={isOpen}
-            placement="right"
-            onClose={onClose}
-            finalFocusRef={myRef}
+const SlideDrawer = ({ isOpen, onClose, focusRef }) => {
+  return (
+    <Drawer
+      isOpen={isOpen}
+      placement="right"
+      onClose={onClose}
+      finalFocusRef={focusRef}
+      size="xlg"
+      sx={{ backgroundColor: "purple" }}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerHeader
+          sx={{
+            backgroundColor: "var(--primary-purple)",
+            color: "white",
+            float: "right",
+          }}
         >
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerHeader>Create your account</DrawerHeader>
+          <Link onClick={onClose} to="/">
+            STEMEY.
+          </Link>
+        </DrawerHeader>
 
-                <DrawerBody>
-                </DrawerBody>
-
-                <DrawerFooter>
-                </DrawerFooter>
-            </DrawerContent>
-
-        </Drawer>
-    )
-}
+        <DrawerBody
+          className="drawer-body"
+          sx={{ backgroundColor: "var(--primary-purple)" }}
+        >
+          <ul className="drawer-list">
+            <li className="drawer-list-item">
+              <p>Academics</p>
+              <div className="drawer-list-links-container">
+                <Link onClick={onClose} to="/classes">
+                  Classes
+                </Link>
+                <Link onClick={onClose} to="/tutoring">
+                  Tutoring
+                </Link>
+                <Link onClick={onClose} to="/tutoring-summer">
+                  Tutoring Summer Program
+                </Link>
+              </div>
+            </li>
+            <li className="drawer-list-item">
+              <p>Events</p>
+              <div className="drawer-list-links-container">
+                <Link onClick={onClose} to="/volunteer">
+                  Volunteer
+                </Link>
+                <Link onClick={onClose} to="/community">
+                  Join Our Community
+                </Link>
+                <Link onClick={onClose} to="/partner">
+                  Partner With Us
+                </Link>
+              </div>
+            </li>
+            <li className="drawer-list-item">
+              <p>Get Involved</p>
+              <div className="drawer-list-links-container">
+                <Link onClick={onClose} to="/stemworld">
+                  STEM World I
+                </Link>
+                <Link onClick={onClose} to="/stemworld2">
+                  STEM World II
+                </Link>
+                <Link onClick={onClose} to="/test">
+                  STEM-Tober
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 SlideDrawer.propTypes = {
-    open: PropTypes.bool.isRequired
-}
+  open: PropTypes.bool.isRequired,
+};
 
 /**
  
@@ -80,4 +135,4 @@ SlideDrawer.propTypes = {
 
  */
 
-export default SlideDrawer
+export default SlideDrawer;
