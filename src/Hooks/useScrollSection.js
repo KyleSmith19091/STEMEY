ece624c1f02529876fe35b13cbc3a5fae8fa1413
Kyle Smith <sKorpion19091@gmail.com>
2021-07-17 17:00:10 +0200

    🧹 Added Comments

--- src/Hooks/useScrollSection.js
+++ src/Hooks/useScrollSection.js
@@ -1,24 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useScrollSection = (sectionClass) => {
-	const [scroll, setScroll] = useState(0);
-	const [sectionHeight, setSectionHeight] = useState(0);
+  const [scroll, setScroll] = useState(0);
+  const [sectionHeight, setSectionHeight] = useState(0);
 
-	const handleScroll = () => {
-		const totalScroll = window.pageYOffset || document.documentElement.scrollTop;
-		const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-		const s = `${totalScroll / windowHeight}`;
-		setScroll(s * 100);
-	};
+  const handleScroll = () => {
+    const totalScroll =
+      window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const s = `${totalScroll / windowHeight}`;
+    setScroll(s * 100);
+  };
 
-	useEffect(() => {
-		window.addEventListener("scroll", handleScroll);
-		document.documentElement.style.setProperty('--card-height', document.querySelector(".timeline-card-expanded").clientHeight);
-		setSectionHeight(document.querySelector(".stemworld").clientHeight / document.documentElement.scrollHeight * 100);
-		return () => window.removeEventListener("scroll", handleScroll); // Same as component did unmount
-	}, []);
+  useEffect(() => {
+    // Add scroll event listener to window
+    window.addEventListener("scroll", handleScroll);
 
-	return {
-		scroll: scroll, sectionHeight: sectionHeight
-	}
-}
+    // Get card height after dom is loaded
+    document.documentElement.style.setProperty(
+      "--card-height",
+      document.querySelector(".timeline-card-expanded").clientHeight
+    );
+
+    // Set sectionheight state
+    setSectionHeight(
+      (document.querySelector(".stemworld").clientHeight /
+        document.documentElement.scrollHeight) *
+        100
+    );
+
+    // Cleanup scroll event listener
+    return () => window.removeEventListener("scroll", handleScroll); // Same as component did unmount
+  }, []);
+
+  return {
+    scroll: scroll,
+    sectionHeight: sectionHeight,
+  };
+};
