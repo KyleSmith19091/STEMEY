abb0f56e05acc082c39f1ad6b56c35f3b2dfccfb
Kyle Smith <sKorpion19091@gmail.com>
2021-07-17 17:01:17 +0200

    ⚡️ Fixed card positioning

--- src/Component/ExpandedTimelineCard.jsx
+++ src/Component/ExpandedTimelineCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
 // External Components
@@ -11,55 +11,69 @@ import { ReactComponent as Calendar } from "../Img/Icon/Calendar.svg";
 // CSS
 import "../Style/Component/ExpandedTimelineCard.css";
 
-const ExpandedTimelineCard = ({ open, setOpen, absolute, content }) => {
-	return (
-		<div
-			className={!absolute ? "timeline-card-expanded" : "timeline-card-expanded absolute"}
-			style={{ display: open ? "flex" : "none" }}
-			onClick={(e) => {
-				setOpen(!open);
-			}}
-		>
-			<div className="card-header">
-				<div className="overlapped-images">
-					<img src={content.eventImage} alt="Event" />
-					<img src={content.speakerImage} alt="Speaker" />
-				</div>
-				<div className="text-container">
-					<h2>{content.title}</h2>
-				</div>
-			</div>
-			<div className="info-container">
-				<p>
-					<span>
-						<Profile />
-					</span>
-					{content.speaker}
-				</p>
-				<p>
-					<span>
-						<Calendar />
-					</span>
-					{content.date}
-				</p>
-			</div>
-			<div className="description-container">
-				<p>
-					{content.description}
-				</p>
-			</div>
-			<div className="video-container">
-				<AspectRatio ratio={16 / 9}>
-					<iframe title="naruto" src={`https://www.youtube.com/embed/${content.videoID}`} allowFullScreen />
-				</AspectRatio>
-			</div>
-		</div>
-	)
-}
+const ExpandedTimelineCard = ({
+  open,
+  setOpen,
+  absolute,
+  initial,
+  content,
+}) => {
+  return (
+    <div
+      className={
+        !absolute
+          ? initial
+            ? "timeline-card-expanded initial"
+            : "timeline-card-expanded"
+          : "timeline-card-expanded absolute"
+      }
+      style={{ display: open ? "flex" : "none" }}
+      onClick={(e) => {
+        setOpen(!open);
+      }}
+    >
+      <div className="card-header">
+        <div className="overlapped-images">
+          <img src={content.eventImage} alt="Event" />
+          <img src={content.speakerImage} alt="Speaker" />
+        </div>
+        <div className="text-container">
+          <h2>{content.title}</h2>
+        </div>
+      </div>
+      <div className="info-container">
+        <p>
+          <span>
+            <Profile />
+          </span>
+          {content.speaker}
+        </p>
+        <p>
+          <span>
+            <Calendar />
+          </span>
+          {content.date}
+        </p>
+      </div>
+      <div className="description-container">
+        <p>{content.description}</p>
+      </div>
+      <div className="video-container">
+        <AspectRatio ratio={16 / 9}>
+          <iframe
+            title="naruto"
+            src={`https://www.youtube.com/embed/${content.videoID}`}
+            allowFullScreen
+          />
+        </AspectRatio>
+      </div>
+    </div>
+  );
+};
 
 ExpandedTimelineCard.propTypes = {
-	open: PropTypes.bool.isRequired,
-	absolute: PropTypes.bool.isRequired
-}
+  open: PropTypes.bool.isRequired,
+  absolute: PropTypes.bool.isRequired,
+};
 
-export default ExpandedTimelineCard
+export default ExpandedTimelineCard;
