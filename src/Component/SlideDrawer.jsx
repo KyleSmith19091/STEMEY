2e337e94eea04f41f92a4aa11d9ca19a59700a9f
Kyle Smith <sKorpion19091@gmail.com>
2021-07-27 15:40:55 +0200

    🧹 Prop cleanup

--- src/Component/SlideDrawer.jsx
+++ src/Component/SlideDrawer.jsx
@@ -96,43 +96,4 @@ const SlideDrawer = ({ isOpen, onClose, focusRef }) => {
   );
 };
 
-SlideDrawer.propTypes = {
-  open: PropTypes.bool.isRequired,
-};
-
-/**
- 
-
-                <ul className="drawer-list">
-                <li className="drawer-list-item">
-                    <p>Academics</p>
-                    <div className="drawer-list-links-container">
-                        <a href="/classes">Classes</a>
-                        <a href="/tutoring">Tutoring</a>
-                        <a href="/tutoring-summer">Tutoring Summer Program</a>
-                    </div>
-                </li>
-                <li className="drawer-list-item">
-                    <p href="/">Events</p>
-                    <div className="drawer-list-links-container">
-                        <a href="/volunteer">Volunteer</a>
-                        <a href="/community">Join Our Community</a>
-                        <a href="/partner">Partner With Us</a>
-                    </div>
-                </li>
-                <li className="drawer-list-item">
-                    <p href="/">Get Involved</p>
-                    <div className="drawer-list-links-container">
-                        <a href="/stemworldI">STEM World I</a>
-                        <a href="/stemworldII">STEM World II</a>
-                        <a href="/stemtober">STEM-Tober</a>
-                    </div>
-                </li>
-            </ul>
-
-
-
-
- */
-
 export default SlideDrawer;
