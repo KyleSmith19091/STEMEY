8813f7ae2c02b467af0b849f76c02b00069a1f83
Kyle Smith <sKorpion19091@gmail.com>
2021-07-29 10:28:08 +0200

    ⭐️ Updated Team card css

--- src/Route/MeetOurTeam.jsx
+++ src/Route/MeetOurTeam.jsx
@@ -2,15 +2,70 @@ import React from "react";
 
 import MemberCard from "../Component/MemberCard";
 
+import Profile from "../Img/Photo/elyssa_wolter.png";
+
+import "../Style/Route/MeetOurTeam.css";
+
 const MeetOurTeam = () => {
   return (
-    <MemberCard
-      name="John"
-      position="CEO"
-      para="lorem ipsum, Faster than fast, quicker than quick. I am Lightning. That was the opening line of the movie cars BTW. 
-  woah lit lit ⚡️🔥"
-      // image={image}
-    />
+    <div className="grid-container team-grid">
+      <MemberCard
+        name="John Smith"
+        position="CEO"
+        para="lorem ipsum, Faster than fast, quicker than quick. I am Lightning. That was the opening line of the movie cars BTW. woah lit lit ⚡️🔥"
+        image={Profile}
+      />
+
+      <MemberCard
+        name="Mary Jane"
+        position="CFO"
+        para="Navaneet Kadaba is a freshman studying Computer Science at Georgia Institute of Technology. He is orginally from the Bay Area and graduated from Homestead High School. He is the CTO of STEMEY."
+        image={Profile}
+      />
+
+      <MemberCard
+        name="John Smith"
+        position="CEO"
+        para="lorem ipsum, Faster than fast, quicker than quick. I am Lightning. That was the opening line of the movie cars BTW. woah lit lit ⚡️🔥"
+        image={Profile}
+      />
+
+      <MemberCard
+        name="Mary Jane"
+        position="CFO"
+        para="Navaneet Kadaba is a freshman studying Computer Science at Georgia Institute of Technology. He is orginally from the Bay Area and graduated from Homestead High School. He is the CTO of STEMEY."
+        image={Profile}
+      />
+
+      <MemberCard
+        name="John Smith"
+        position="CEO"
+        para="lorem ipsum, Faster than fast, quicker than quick. I am Lightning. That was the opening line of the movie cars BTW. woah lit lit ⚡️🔥"
+        image={Profile}
+      />
+
+      <MemberCard
+        name="Mary Jane"
+        position="CFO"
+        para="Navaneet Kadaba is a freshman studying Computer Science at Georgia Institute of Technology. He is orginally from the Bay Area and graduated from Homestead High School. He is the CTO of STEMEY."
+        image={Profile}
+      />
+
+      <MemberCard
+        name="John Smith"
+        position="CEO"
+        para="lorem ipsum, Faster than fast, quicker than quick. I am Lightning. That was the opening line of the movie cars BTW. woah lit lit ⚡️🔥"
+        image={Profile}
+      />
+
+      <MemberCard
+        name="Mary Jane"
+        position="CFO"
+        para="Navaneet Kadaba is a freshman studying Computer Science at Georgia Institute of Technology. He is orginally from the Bay Area and graduated from Homestead High School. He is the CTO of STEMEY."
+        image={Profile}
+      />
+
+    </div>
   );
 };
 
