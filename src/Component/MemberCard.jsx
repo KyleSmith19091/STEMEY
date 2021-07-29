f36cfef656b249ad15ca71bc9c5675602ed538e7
Kyle Smith <sKorpion19091@gmail.com>
2021-07-29 10:28:25 +0200

    ⭐️ Updated Team card CSS

--- src/Component/MemberCard.jsx
+++ src/Component/MemberCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-import styles from "../Style/Component/MeetOurTeam.module.css";
+import styles from "../Style/Component/MeetOurTeam.css";
 
 const MemberCard = ({ para, name, position, image }) => {
   return (
-    <div className={styles.meetOurTeamCard}>
-      <div className={styles.meetOurTeamCardInner}>
-        <div className={styles.meetOurTeamCardFront}>
-          <img className={styles.memberPhoto} src={image} alt="member photo" />
-          <div className={styles.title}>{name}</div>
+    <div className="meetOurTeamCard">
+      <div className="meetOurTeamCardInner">
+        <div className="meetOurTeamCardFront">
+          <img className="memberPhoto" src={image} alt="member photo" />
+          <div className="member-title">{name}</div>
         </div>
-        <div class={styles.meetOurTeamCardBack}>
-          <h1 className={styles.title}>{position}</h1>
+        <div class="meetOurTeamCardBack">
+          <h1 className="member-title">{position}</h1>
           <p>{para}</p>
-          <h1 className={styles.title}>{name}</h1>
+          <h1 className="member-title">{name}</h1>
         </div>
       </div>
     </div>
